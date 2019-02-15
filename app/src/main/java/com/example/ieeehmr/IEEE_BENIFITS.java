a153e495b6daee64530c58e67f3fd7b39443c881
Lovish <lovishkaushik1@gmail.com>
2019-02-15 23:24:38 +0530

    IMAGE SLIDER IS IMPLEMENTED LAYOUYT COLOUR IS CHANGED TO BLUE IN ACTIVITY BENIFITS OF IEEE , YOUTUBE API IS ADDED TO PLAY YOUTUBE VIDEOS A PLAY BUTTON IS ALSO INTRODUCED OVER VIDEO WHOSE VISIBLITY HIDES ONCLICK

--- app/src/main/java/com/example/ieeehmr/IEEE_BENIFITS.java
+++ app/src/main/java/com/example/ieeehmr/IEEE_BENIFITS.java
@@ -2,12 +2,50 @@ package com.example.ieeehmr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class IEEE_BENIFITS extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class IEEE_BENIFITS extends YouTubeBaseActivity {
+    ImageView imageView;
+
+    YouTubePlayerView youTubePlayerView;
+    TextView textView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ieee__benifits);
+        imageView=findViewById(R.id.playButton);
+        textView=findViewById(R.id.tv);
+        youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtubePlayerView);
+        onInitializedListener= new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("wJyQwLfPtZ4");
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youTubePlayerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setVisibility(View.GONE);
+                textView.setText(" ");
+                youTubePlayerView.initialize(playerConfig.API_KEY,onInitializedListener);
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/ieeehmr/playerConfig.java
@@ -0,0 +1,6 @@
+package com.example.ieeehmr;
+
+public class playerConfig {
+    playerConfig(){}
+    public static final String API_KEY="AIzaSyAGX1ESbrQWqI95SonJmRGNomu0UmDYHuw";
+}
